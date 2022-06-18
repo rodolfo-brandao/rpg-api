65e83ca01c64afe23fe7025f568916b7c3cd504c
Rodolfo <rodolfobrandao1995@gmail.com>
2022-06-18 15:43:56 -0300

    Add utility classes

---
+++ Rpg.Application/Util/ApiVersions.cs
@@ -0,0 +1,10 @@
+﻿namespace Rpg.Application.Util
+{
+    /// <summary>
+    /// A collection of API version constants, using the pattern "v{#}".
+    /// </summary>
+    public static class ApiVersions
+    {
+        public const string V1 = "v1";
+    }
+}
---
+++ Rpg.Application/Util/ContentTypes.cs
@@ -0,0 +1,10 @@
+﻿namespace Rpg.Application.Util
+{
+    /// <summary>
+    /// A collection of constants for Content-Types.
+    /// </summary>
+    public static class ContentTypes
+    {
+        public const string Json = "application/json";
+    }
+}
