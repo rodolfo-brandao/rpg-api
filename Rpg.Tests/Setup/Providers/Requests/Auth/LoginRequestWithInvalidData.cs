c42cc392eca85d1109427ece5506b9af372815c8
Rodolfo <rodolfobrandao1995@gmail.com>
2022-06-26 11:19:37 -0300

    Add data provider for login request

---
+++ Rpg.Tests/Setup/Providers/Requests/Auth/LoginRequestWithInvalidData.cs
@@ -0,0 +1,23 @@
+﻿using Rpg.Tests.Setup.Fakers.Requests.Auth;
+using System.Collections;
+
+namespace Rpg.Tests.Setup.Providers.Requests.Auth
+{
+    internal class LoginRequestWithInvalidData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[]
+            {
+                LoginRequestFake.WithNoUsername.Generate()
+            };
+
+            yield return new object[]
+            {
+                LoginRequestFake.WithNoPassword.Generate()
+            };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
